af991e64421b2b4bc7eac83cb7205e64dafe4155
Longlone05 <duylongbmta1@gmail.com>
2024-12-06 15:39:00 +0700

    Update Findthemaximumsinglesellprofit.py

--- Findthemaximumsinglesellprofit.py
+++ Findthemaximumsinglesellprofit.py
@@ -1,11 +1,11 @@
 n = list(map(int, input("Nhập giá cổ phiếu: ").split()))
 
 def max_profit(stock_price):
-    maxP = 0 
-    for i in range(len(stock_price)):
-        for j in range(i + 1, len(stock_price)):
-            profit = stock_price[j] - stock_price[i]  # Tính lợi nhuận
-            if profit > maxP:
-                maxP = profit
-    return maxP
+    max_profit = 0  # Khởi tạo giá trị lợi nhuận tối đa ban đầu là rất nhỏ
+    for i in range(1, len(stock_price)):
+        profit = stock_price[i] - stock_price[i-1]  # Tính sự chênh lệch giữa số bên phải và số bên trái
+        max_profit = max(max_profit, profit)  # Cập nhật lợi nhuận tối đa
+    
+    return max_profit
+
 print("Lợi nhuận tối đa có thể đạt được:", max_profit(n))
